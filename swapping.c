3c5d3b8d9cff68cfeaac64a0f499ad48eadbbde1
pranav301999 <33198168+pranav301999@users.noreply.github.com>
2017-12-22 01:43:43 +0530

    Update swapping.c

--- swapping.c
+++ swapping.c
@@ -5,7 +5,8 @@ void main()
 int a,b,c,d,f;
 printf("Enter Two no's:");
   printf("Enter Two no's:");
-d=a;
+d=a;teri maa ki chute;
+  madarchod;
 a=b;
 b=d;
 printf("The two no's after swapping are:%d%d",a,b);
